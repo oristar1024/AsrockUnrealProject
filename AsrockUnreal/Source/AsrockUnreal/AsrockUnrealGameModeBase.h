b04921fe1157fef0cdf09555ff0b120c636f8d4a
oristar1024 <tronia94@gmail.com>
2023-06-14 12:10:05 +0900

    Create Empty UnrealProject

---
+++ AsrockUnreal/Source/AsrockUnreal/AsrockUnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AsrockUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASROCKUNREAL_API AAsrockUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
